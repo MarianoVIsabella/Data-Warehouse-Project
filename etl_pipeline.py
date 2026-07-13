05214558c5dd4fa58ebcc110261a4f5fe7aa0edf
MarianoVIsabella <marianovisabella@libero.it>
2026-07-13 20:12:35 +0200

    Added schema to follow for etl pipeline

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import psycopg
+#Extraction: extract what you need from world_cups_recdb schema
+#Transformation: 
+# Add the correct Federation to each nation
+# Create the Date -> Year -> Decade -> Century hierarchy for Date Dimension
+# Compute the n_measures
+#Load: Load everything to world_cups_star_schema
